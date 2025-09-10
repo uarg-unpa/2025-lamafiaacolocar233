f510bc896fe6ecb7d71a3e54716bb7995026d980
lamafiaacolocar233 <150186107+lamafiaacolocar233@users.noreply.github.com>
2025-09-10 17:59:52 +0000

    Se sube tp1 y tp2

---
+++ tp2I/PuntoUno/Persona.java
@@ -0,0 +1,39 @@
+package tp2;
+
+public class Persona {
+   
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private int dni;
+
+    // Constructor
+    public Persona(String nombre, String apellido, int edad, int dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+   
+    public String getNombre() { return this.nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getApellido() { return this.apellido; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
+
+    public int getEdad() { return this.edad; }
+    public void setEdad(int edad) { this.edad = edad; }
+
+    public int getDni() { return this.dni; }
+    public void setDni(int dni) { this.dni = dni; }
+
+  
+    public boolean esMayorDeEdad() {
+        return this.edad >= 18;
+    }
+
+    public String toString() {
+        return this.nombre + " " + this.apellido + " | Edad: " + this.edad + " | DNI: " + this.dni;
+    }
+}
